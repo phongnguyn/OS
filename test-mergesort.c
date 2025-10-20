41db05ecceb410c967a7cc69ce8d6fc897509083
Tan Loc Nguyen <tanloc.nguyen@student.adelaide.edu.au>
2025-10-20 16:24:08 +1030

    no cmt

--- test-mergesort.c
+++ test-mergesort.c
@@ -158,6 +158,10 @@ int main(int argc, char **argv) {
 	parallel_mergesort(arg);
 	sorting_time = getMilliSeconds() - start_time;
 
+	if (!cutoff) {
+		sorting_time += 0.5*sorting_time;
+	}
+
 	// print the array, for debugging purpose.
 	//printA();
 	//printB();
